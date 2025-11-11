0ea8ce8c5061791a899b92ada7fabf9b8a76b0e9
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-11-11 15:47:21 +0800

    lesson 9 : 1: Basic range loop

---
+++ Lesson09_For_Loops.py
@@ -0,0 +1,6 @@
+# LESSON 9: FOR LOOPS
+# For loops are used to repeat code for a specific number of times or over a sequence.
+
+# Example 1: Basic range loop
+for i in range(5):
+    print("Count:", i)
