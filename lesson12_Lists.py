128c9f4400273843400070e1061b7724e18977e2
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-11-12 01:34:59 +0800

    lesson 12 : 10) List comprehension

--- lesson12_Lists.py
+++ lesson12_Lists.py
@@ -40,3 +40,8 @@ for f in fruits:
 # Sorting list
 fruits.sort()
 print(fruits)  # Output: ['apple', 'grape', 'mango']
+
+
+# List comprehension
+squares = [x**2 for x in range(5)]
+print(squares)  # Output: [0, 1, 4, 9, 16]
