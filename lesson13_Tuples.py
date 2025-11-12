12d5a11291da6cbf1ef9c8497488614a174a677a
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-11-13 01:55:31 +0800

    lesson 13 : 6) Tuple concatenation

--- lesson13_Tuples.py
+++ lesson13_Tuples.py
@@ -15,3 +15,8 @@ print(nested[1][1])  # Output: 3
 # Loop through tuple
 for c in colors:
     print(c)  # Output: red \n green \n blue
+
+# Tuple concatenation
+t1 = (1, 2)
+t2 = (3, 4)
+print(t1 + t2)  # Output: (1, 2, 3, 4)
