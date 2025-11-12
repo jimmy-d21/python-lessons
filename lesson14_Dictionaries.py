81060b07c7254e83d4ad9b5d5792926ed638e746
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-11-13 01:57:36 +0800

    lesson 14 : 2) Accessing values

--- lesson14_Dictionaries.py
+++ lesson14_Dictionaries.py
@@ -1,3 +1,6 @@
 # Creating a dictionary
 person = {"name": "Marjani", "age": 22, "country": "Tanzania"}
 print(person)  # Output: {'name': 'Marjani', 'age': 22, 'country': 'Tanzania'}
+
+# Accessing values
+print(person["name"])  # Output: Marjani
