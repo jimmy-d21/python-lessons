7e273bfb04f2b0c66937047df510399362f598c2
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-11-17 00:10:53 +0800

    lesson 17 : 4) Continue statement

--- lesson17_loops.py
+++ lesson17_loops.py
@@ -13,3 +13,9 @@ for i in range(5):
     if i == 3:
         break
     print(i)  # Output: 0 1 2
+
+# Continue statement
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)  # Output: 0 1 3 4
