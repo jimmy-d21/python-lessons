be7e6c9d392d9c5c771c788b8f3b4d53f91f9c7d
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-11-17 00:16:12 +0800

    lesson 18 : 2) Read entire file

--- lesson18_file_handling.py
+++ lesson18_file_handling.py
@@ -1,3 +1,7 @@
 # File Handling
 with open("demo.txt", "w") as f:
     f.write("Hello, File Handling!")  # Output: File created with text
+
+# File Handling
+with open("demo.txt", "r") as f:
+    print(f.read())  # Output: Hello, File Handling!
