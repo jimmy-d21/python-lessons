3c055340c40ac4f47da605c99f596d908f08d8e9
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-11-18 23:54:04 +0800

    lesson 19 : 3) Class with multiple attributes

--- lesson19_Classes_&_Objects.py
+++ lesson19_Classes_&_Objects.py
@@ -12,3 +12,18 @@ print(p.name)  # Output: Marjani
 class Greeter:
     def greet(self):
         print("Hello!")  # Output: Hello!
+
+
+g = Greeter()
+g.greet()
+
+
+# Classes
+class Car:
+    def __init__(self, brand, year):
+        self.brand = brand
+        self.year = year
+
+
+mycar = Car("Toyota", 2023)
+print(mycar.brand, mycar.year)  # Output: Toyota 2023
