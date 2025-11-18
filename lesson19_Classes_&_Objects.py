6f3ebb3fd736af4f97d80d90304f618411f85f44
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-11-18 23:53:48 +0800

    lesson 19 : 2) Method inside class

--- lesson19_Classes_&_Objects.py
+++ lesson19_Classes_&_Objects.py
@@ -2,3 +2,13 @@
 class Person:
     def __init__(self, name):
         self.name = name
+
+
+p = Person("Marjani")
+print(p.name)  # Output: Marjani
+
+
+# Classes
+class Greeter:
+    def greet(self):
+        print("Hello!")  # Output: Hello!
