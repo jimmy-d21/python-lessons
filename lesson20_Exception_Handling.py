5b57e2eb824e5db77607e44ee402ca584a41bc3f
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-11-19 23:35:35 +0800

    lesson 20 : 9) Catch all

--- lesson20_Exception_Handling.py
+++ lesson20_Exception_Handling.py
@@ -60,3 +60,10 @@ except:
 x = 5
 assert x > 0, "x must be positive"
 print("Valid")  # Output: Valid
+
+
+# Exception
+try:
+    undefined
+except Exception as e:
+    print("Error:", e)  # Output: name 'undefined' is not defined
