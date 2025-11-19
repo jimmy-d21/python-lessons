50fa44617999a3e3288139e94f0d0230f90ffcce
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-11-19 23:39:28 +0800

    Comprehension 22 : 3) String uppercase

--- lesson22_List_Comprehension.py
+++ lesson22_List_Comprehension.py
@@ -6,3 +6,9 @@ print(nums)  # Output: [0,1,4,9,16]
 # List
 evens = [x for x in range(10) if x % 2 == 0]
 print(evens)  # Output: [0,2,4,6,8]
+
+
+# List
+names = ["marjani", "eilert"]
+upper = [n.upper() for n in names]
+print(upper)  # Output: ['MARJANI','EILERT']
