1f353eca89505df44a07a6a6d5486f94374a2859
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-11-19 23:37:17 +0800

    lesson 21 : 5) Custom module example

--- lesson21_Modules.py
+++ lesson21_Modules.py
@@ -16,3 +16,9 @@ print(m.pi)  # Output: 3.141592...
 
 # Modules
 print(sin(0), cos(0))  # Output: 0 1
+
+
+# Modules
+# mymodule.py → def hello(): print("Hi!")
+# import mymodule
+# mymodule.hello()  # Output: Hi!
