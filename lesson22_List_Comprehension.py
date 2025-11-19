65389cb30f9588e0c6332963c8cd890938dde8b0
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-11-19 23:39:19 +0800

    Comprehension 22 : 2) Even numbers

--- lesson22_List_Comprehension.py
+++ lesson22_List_Comprehension.py
@@ -1,3 +1,8 @@
 # List Comprehension
 nums = [x*x for x in range(5)]
 print(nums)  # Output: [0,1,4,9,16]
+
+
+# List
+evens = [x for x in range(10) if x % 2 == 0]
+print(evens)  # Output: [0,2,4,6,8]
