a5185e9657889ef09a66ea626cd9bb87c73d91b1
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-11-19 23:34:02 +0800

    lesson 20 : 2) Multiple except

--- lesson20_Exception_Handling.py
+++ lesson20_Exception_Handling.py
@@ -3,3 +3,10 @@ try:
     print(10/0)
 except ZeroDivisionError:
     print("Cannot divide by zero!")  # Output: Cannot divide by zero!
+
+
+# Exception
+try:
+    int("abc")
+except ValueError:
+    print("Invalid number!")  # Output: Invalid number!
